27a886da15cb4894f97fae06a7ebf1c8ee95d342
SLW développement <111683903+Badsty@users.noreply.github.com>
2022-08-21 13:30:09 +0200

    porter
    SLW développement script porter

---
+++ porter/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'badsty#7436'
+description 'SLW développement'
+version '1.0.0'
+
+client_script "cl_carry.lua"
+server_script "sv_carry.lua"
